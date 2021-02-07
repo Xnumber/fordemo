ad12fbf4f12570faca5aa2fb01595cc089877bc8
Clement Syu <clement.syu.csf@gmail.com>
2021-02-07 17:29:09 +0800

    conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+
+window.React = React;
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
 					className="App-link"
@@ -15,7 +17,7 @@ Edit <code>src/App.js</code> and save to reload.
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-Learn React
+          Learn React
 				</a>
 			</header>
 		</div>
